2d7d1672e6274494079febe75998714e0e48a9e4
jennysujukim <jennysujukim@gmail.com>
2023-07-31 01:13:17 -0700

    Refactor server.js cors

--- server/server.js
+++ server/server.js
@@ -15,15 +15,9 @@ const PORT = process.env.REACT_APP_PORT || 5000;
 app.use(bodyParser.json());
 
 // enable CORS
-app.use(cors());
-
-// restrict webpages from making requests to a different domain
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-    next();
-})
+app.use(cors({
+    origin: [`http://localhost:${PORT}`, "http://localhost:3000", "https://daily-protein.seojeongkim.com" ]
+}));
 
 // routes
 app.use('/api/profile', profileRoutes);
